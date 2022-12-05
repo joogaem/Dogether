c4fa7aa0f733b9ce503bca08bba90fc9d3f2d8dc
OTecHeun <tjdgms1145a@naver.com>
2022-12-06 00:01:43 +0900

    'check'

--- src/container/MedicineContent.jsx
+++ src/container/MedicineContent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../resources/assets/sass/medicine.scss';
-function MedicineContent(props) {
 
+function MedicineContent(props) {
     var data = [
         {
             image : "사진",
@@ -32,6 +32,9 @@ function MedicineContent(props) {
             <div className='menuTitle'>투약의뢰서</div>
             <div className='menuTitleSub'>금일 우리 아이의 투약을 선생님께 의뢰합니다.</div>
         </div>
+        <div className='buttons'>
+          <button className='pencil'>작성하기</button>
+        </div>
         <table>
         <tbody>
           {data.map(({ image, check, name, classs, date }, idx) => (
@@ -44,7 +47,12 @@ function MedicineContent(props) {
                 </div>
               </td>
               <td className='date'>{date}</td>
-              <td className='check'><input type="checkbox"></input></td>
+              <td className='check'>
+                <label>
+                  <input className='checkInput' type="checkbox" id='ck' />
+                  <span className='customcheckBox'></span>
+                </label>
+              </td>
             </tr>
           ))}
         </tbody>
